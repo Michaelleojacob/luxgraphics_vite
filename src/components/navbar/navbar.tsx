3e89d5800ea64f02dd6c9b71020489c239402165
mig <michaelleojacob@gmail.com>
2023-06-16 11:18:36 -0700

    updated tailwind, made sure its being applied. used chatgpt install steps instead of the vite steps from tailwind docs

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,15 +2,9 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/">home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/test">test</NavLink>
-        </li>
-      </ul>
+    <div className="flex gap-4">
+      <NavLink to="/">home</NavLink>
+      <NavLink to="/test">test</NavLink>
     </div>
   );
 };
