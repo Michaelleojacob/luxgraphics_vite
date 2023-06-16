efa0fd039426bd1cc8bf78acaf8b50b04882eafa
mig <michaelleojacob@gmail.com>
2023-06-16 10:58:50 -0700

    +added shopping cart context, and began working on basic routing

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <NavLink to="/">home</NavLink>
+      <NavLink to="/">home</NavLink>
+      <NavLink to="/">home</NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
